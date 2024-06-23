22db6f724ef55261bdd2eb5ab03d5aff075c6c09
hatef khodkar <hatef.khodkar@hotmail.com>
2024-06-23 14:28:22 +0400

    chore(tailwind): remove unused import and variables from tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-import { colord, extend } from "colord";
-import mixPlugin from "colord/plugins/mix";
 import themeColors from "./src/theme/colors";
 
-extend([mixPlugin]);
-const generateDarkenColorFrom = (input, percentage = 0.07) =>
-  colord(input).darken(percentage).toHex();
-const generateForegroundColorFrom = (input, percentage = 0.08) =>
-  colord(input)
-    .mix(colord(input).isDark() ? "white" : "black", percentage)
-    .toHex();
 
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
