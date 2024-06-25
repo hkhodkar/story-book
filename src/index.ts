31d7ba311d9d44bc4a644b06b2ac94e53a2a24ac
hatef khodkar <hatef.khodkar@hotmail.com>
2024-06-25 19:25:06 +0400

    fix(input): add input in npm package

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as themeColors } from "./theme/colors";
 export { default as Button } from "./stories/button/button";
 export { default as Loading } from "./stories/loading/loading";
+export { default as Input } from "./stories/input/input";
 import "./index.scss";
