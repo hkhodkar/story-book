77ca57bba4a8d20bd49d7ff24af1fa14f81588e9
hatef khodkar <hatef.khodkar@hotmail.com>
2024-06-27 14:25:29 +0400

    fix(checkbox): add checkbox component to the output of npm package

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export { default as themeColors } from "./theme/colors";
 export { default as Button } from "./stories/button/button";
 export { default as Loading } from "./stories/loading/loading";
 export { default as Input } from "./stories/input/input";
+export { default as Checkbox } from "./stories/checkbox/checkbox";
 import "./index.scss";
