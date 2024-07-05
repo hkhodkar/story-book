9ffeb6a6d692062f6b0125684c87053e4954e098
hatef khodkar <hatef.khodkar@hotmail.com>
2024-07-05 20:55:28 +0400

    fix(report card): add report card to npm

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export { default as Button } from "./stories/button/button";
 export { default as Loading } from "./stories/loading/loading";
 export { default as Input } from "./stories/input/input";
 export { default as Checkbox } from "./stories/checkbox/checkbox";
+export { default as ReportCard } from "./stories/reportCard/reportCard";
 import "./index.scss";
