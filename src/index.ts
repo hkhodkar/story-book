04079b022525f992faebcc583b1aeedf02d9a34f
hatef khodkar <hatef.khodkar@hotmail.com>
2024-07-12 17:21:38 +0400

    chore(refactor): refactor code

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ export { default as Button } from "./stories/button/button";
 export { default as Loading } from "./stories/loading/loading";
 export { default as Input } from "./stories/input/input";
 export { default as Checkbox } from "./stories/checkbox/checkbox";
-export { default as ReportCard } from "./stories/reportCard/reportCard";
+export { default as ReportCard } from "./stories/report-card/reportCard";
 export { default as Select } from "./stories/select/select";
 export { default as ExpansionPanel } from "./stories/expansion-panel/expansion-panel";
 export { default as Pagination } from "./stories/pagination/pagination";
