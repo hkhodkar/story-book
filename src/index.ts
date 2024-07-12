aa094e7acea4a6949ee8aa62556da0aafeead58e
hatef khodkar <hatef.khodkar@hotmail.com>
2024-07-12 11:58:43 +0400

    fix: add select and expansion panel to npm package

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,6 @@ export { default as Loading } from "./stories/loading/loading";
 export { default as Input } from "./stories/input/input";
 export { default as Checkbox } from "./stories/checkbox/checkbox";
 export { default as ReportCard } from "./stories/reportCard/reportCard";
+export { default as Select } from "./stories/select/select";
+export { default as ExpansionPanel } from "./stories/expansion-panel/expansion-panel";
 import "./index.scss";
